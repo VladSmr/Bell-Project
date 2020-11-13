d549eb222b7bce57d14a6a35d5e5500ae8842437
VladSmr <bio.smir16@gmail.com>
2020-11-13 09:24:17 +0300

    Убран закомментированный код

--- src/main/java/com/bell/project/SwaggerConfiguration.java
+++ src/main/java/com/bell/project/SwaggerConfiguration.java
@@ -20,6 +20,5 @@ public class SwaggerConfiguration {
         List<LinkDiscoverer> plugins = new ArrayList<>();
         plugins.add(new CollectionJsonLinkDiscoverer());
         return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
-
     }
 }
