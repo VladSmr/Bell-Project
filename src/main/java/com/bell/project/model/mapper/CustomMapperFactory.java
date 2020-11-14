86124721f173c0b5ad1063fc36161c867e02c7d2
VladSmr <bio.smir16@gmail.com>
2020-11-14 12:18:31 +0300

    Добавлен маппинг для классов User - UserViewUpdate

--- src/main/java/com/bell/project/model/mapper/CustomMapperFactory.java
+++ src/main/java/com/bell/project/model/mapper/CustomMapperFactory.java
@@ -6,6 +6,7 @@ import com.bell.project.view.office.OfficeFilter;
 import com.bell.project.view.office.OfficeViewSave;
 import com.bell.project.view.user.UserView;
 import com.bell.project.view.user.UserViewSave;
+import com.bell.project.view.user.UserViewUpdate;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.springframework.beans.factory.FactoryBean;
@@ -34,6 +35,14 @@ public class CustomMapperFactory implements FactoryBean<MapperFactory> {
         factory.classMap(User.class, UserViewSave.class)
                 .field("office.id", "officeId")
                 .field("document.documentType.code", "docCode")
+                .field("document.documentType.name", "docName")
+                .field("document.number", "docNumber")
+                .field("document.date", "docDate")
+                .field("nationality.code", "citizenshipCode")
+                .byDefault()
+                .register();
+        factory.classMap(User.class, UserViewUpdate.class)
+                .field("office.id", "officeId")
                 .field("document.documentType.name", "docName")
                 .field("document.number", "docNumber")
                 .field("document.date", "docDate")
