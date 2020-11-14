f1ae768ce786bb84f1f33a67a90054eec4387c89
VladSmr <bio.smir16@gmail.com>
2020-11-14 11:50:54 +0300

    Добавлено поле в маппинг User - UserView

--- src/main/java/com/bell/project/model/mapper/CustomMapperFactory.java
+++ src/main/java/com/bell/project/model/mapper/CustomMapperFactory.java
@@ -5,6 +5,7 @@ import com.bell.project.model.User;
 import com.bell.project.view.office.OfficeFilter;
 import com.bell.project.view.office.OfficeViewSave;
 import com.bell.project.view.user.UserView;
+import com.bell.project.view.user.UserViewSave;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.springframework.beans.factory.FactoryBean;
@@ -27,6 +28,15 @@ public class CustomMapperFactory implements FactoryBean<MapperFactory> {
                 .field("document.number", "documentNumber")
                 .field("document.date", "documentDate")
                 .field("nationality.name", "citizenshipName")
+                .field("nationality.code", "citizenshipCode")
+                .byDefault()
+                .register();
+        factory.classMap(User.class, UserViewSave.class)
+                .field("office.id", "officeId")
+                .field("document.documentType.code", "docCode")
+                .field("document.documentType.name", "docName")
+                .field("document.number", "docNumber")
+                .field("document.date", "docDate")
                 .field("nationality.code", "citizenshipCode")
                 .byDefault()
                 .register();
