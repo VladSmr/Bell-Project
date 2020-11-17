488ce6aa5e43aac28014e300443ff401c647875b
VladSmr <bio.smir16@gmail.com>
2020-11-17 09:57:16 +0300

    ExceptionHandler`ы объединены в один. Добавлен implements ResponseBodyAdvice и методы supports и beforeBodyWrite

--- src/main/java/com/bell/project/controller/ControllersAdvice.java
+++ src/main/java/com/bell/project/controller/ControllersAdvice.java
@@ -1,30 +1,50 @@
 package com.bell.project.controller;
 
+import com.bell.project.Data;
 import com.bell.project.view.ErrorView;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.NoResultException;
 
 @RestControllerAdvice
-public class ControllersAdvice extends ResponseEntityExceptionHandler {
+public class ControllersAdvice extends ResponseEntityExceptionHandler implements ResponseBodyAdvice<Object> {
 
-    @ExceptionHandler({EntityNotFoundException.class, NoResultException.class})
-    protected ResponseEntity<ErrorView> handleEntityNotFoundException() {
-        return new ResponseEntity<>(new ErrorView("There is no such entity in database"), HttpStatus.NOT_FOUND);
+    @ExceptionHandler({Exception.class})
+    protected ResponseEntity<ErrorView> exception(Exception ex) {
+        if (ex.getCause().getClass() == ConstraintViolationException.class) {
+            return new ResponseEntity<>(new ErrorView("Entity with provided ID not found"), HttpStatus.NOT_FOUND);
+        } else if (ex.getCause().getClass() == EntityNotFoundException.class || ex.getCause().getClass() == NoResultException.class) {
+            return new ResponseEntity<>(new ErrorView("There is no such entity in database"), HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(new ErrorView("There is exception " + ex), HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @ExceptionHandler({NullPointerException.class})
-    protected ResponseEntity<ErrorView> handleNullPointerException() {
-        return new ResponseEntity<>(new ErrorView("Check the correct input data"), HttpStatus.NOT_FOUND);
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class aClass) {
+        return true;
     }
 
-    @ExceptionHandler({org.hibernate.exception.ConstraintViolationException.class})
-    protected ResponseEntity<ErrorView> handleConstraintViolationException() {
-        return new ResponseEntity<>(new ErrorView("Entity with provided ID not found"), HttpStatus.NOT_FOUND);
+    @Override
+    public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType,
+                                  Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        if (o == null) {
+            o = "success";
+        }
+        if (o.getClass() == ErrorView.class) {
+            return o;
+        }
+        return new Data<>(o);
     }
 }
